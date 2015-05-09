32fbb82ef376dd58606ff7fba097dc22bb06efbe
fdorr <fran.dorr@gmail.com>
2015-05-09 11:21:05 -0300

    agrego destinatarios a mensaje

--- mensaje.rb
+++ mensaje.rb
@@ -1,10 +1,11 @@
 # Clase que representa a un mensaje
 class Mensaje
-	attr_reader :autor, :contenido	
+	attr_reader :autor, :contenido, :destinatarios	
 
-	def initialize autor, contenido 
+	def initialize autor, contenido, destinatarios 
 		@autor = autor
 		@contenido = contenido
+		@destinatarios = destinatarios
 	end
 	
 end

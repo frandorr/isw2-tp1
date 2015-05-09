3718ef0eeb001a91099fd5cf2734bcc4e0344924
German Romano <romano.german@live.com.ar>
2015-05-09 11:56:03 -0300

    CuerpoDocente pasa self como autor de un mensaje

--- cuerpoEducativo.rb
+++ cuerpoEducativo.rb
@@ -1,8 +1,8 @@
 #Clase que representa a un integrante del Cuerpo Educativo (Docente, Director, Secretario)
 class CuerpoEducativo
 
-	def nuevoMensaje(contenido, destinatario)
-		mensaje = Mensaje.new(contenido, destinatario)
+	def nuevoMensaje(contenido, destinatarios)
+		mensaje = Mensaje.new(self, contenido, destinatarios)
 		return mensaje
 	end
 
